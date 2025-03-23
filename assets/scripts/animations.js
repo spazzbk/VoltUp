9c08e4da326847e5d0177c0b0d164e9eb3710820
spazzbk <151713956+spazzbk@users.noreply.github.com>
2025-03-24 01:06:34 +0300

    Add files via upload

---
+++ assets/scripts/animations.js
@@ -0,0 +1,10 @@
+const sunBlock = document.getElementById("sun");
+
+
+
+animateSunBlock(sunBlock.);
+
+function animateSunBlock(block){
+    const background = block.;
+    console.log(background)
+}
